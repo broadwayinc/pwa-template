ce56c18bfd40309387a65fa956d01de6fc29d9a5
dev <dev@broadwayinc.com>
2025-02-10 01:55:08 +0000

    update

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener('push', function(event) {
         icon: 'icon-192x192.png',
         badge: 'icon-192x192.png'
     };
-
+    
     event.waitUntil(
         self.registration.showNotification(title, options)
     );
